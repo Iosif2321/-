e5bb0499cb59228433c0a6f8742a32f9c25e46f6
Iosif2321 <75369192+Iosif2321@users.noreply.github.com>
2025-06-06 14:46:00 +0300

    Improve TextBoard MP handling

---
+++ TextBoard/CfgFunctions.hpp
@@ -0,0 +1,16 @@
+class CfgFunctions
+{
+    class TextBoard
+    {
+        class Functions
+        {
+            file = "TextBoard\functions";
+            class createBoard{};
+            class inputClosed{};
+            class openInput{};
+            class setText{};
+            class addConsoleAction{};
+            class addDrawHandler{};
+        };
+    };
+};
